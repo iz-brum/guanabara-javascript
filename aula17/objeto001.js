e4f2fead872c2e603f601d09eabc00b481a10fc4
iz-Brum <izann.brum@alunosenai.mt>
2023-09-21 13:10:18 -0400

    Add comentários ao código

--- aula17/objeto001.js
+++ aula17/objeto001.js
@@ -1,15 +1,17 @@
-let animal = {nome: 'Morcego',
-familia: 'Vespertilionidae',
-peso: 8,
-engordar(parametro=0){
-    console.log('engordou');
-    this.peso += parametro;
-}
+// Criação do objeto 'animal' com propriedades nome, familia e peso, 
+// e um método 'engordar'.
+let animal = {
+    nome: 'Morcego',
+    familia: 'Vespertilionidae',
+    peso: 8,
+    engordar(parametro = 0) {   // parametro predeterminado = 0, caso não seja passado valor por parametro
+        console.log('engordou');
+        this.peso += parametro;     // this.peso se refere à propriedade peso do objeto animal.
+    }
 }
 
-animal.engordar(2)
+// Chama o método 'engordar' com um parâmetro de 2.
+animal.engordar(2);
 
+// Exibe uma mensagem com o nome do animal e seu novo peso.
 console.log(`\nNesta semana o ${animal.nome} aumentou o peso para ${animal.peso}g\n`)
-
-//
-/**/ 
