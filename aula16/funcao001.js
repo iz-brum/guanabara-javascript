8e4792c2f26fd33f08039b1a27ca50f87e3f6f74
iz-Brum <izann.brum@alunosenai.mt>
2023-09-21 13:21:33 -0400

    Add comentários ao código

--- aula16/funcao001.js
+++ aula16/funcao001.js
@@ -1,55 +1,58 @@
-// Trabalhando um pocuo com funções
+// Trabalhando um pouco com funções
 
-// Parametro unico 
+// Função para determinar se um número é par ou ímpar
 function parOuImpar(number) {
-    if (number%2==0) {
-        return 'Par'    // retorna uma string
+    if (number % 2 == 0) {
+        return 'Par';   // Retorna uma string indicando que o número é par
     } else {
-        return 'Ímpar'  // retorna uma string também
+        return 'Ímpar'; // Retorna uma string indicando que o número é ímpar
     }
 }
 
-console.log(parOuImpar(10)) 
+// Chama a função e imprime o resultado no console
+console.log(parOuImpar(10));
 
-
-
-
-// Utilizando parametros predefinidos (geralmente utilizado quando a chamada não envia todos os parametros necessários)
-function somarDoisNumeros(number_one=0, number_two=0) { 
-    return number_one + number_two
+// Utilizando parâmetros predefinidos (caso não sejam fornecidos)
+function somarDoisNumeros(number_one = 0, number_two = 0) { 
+    return number_one + number_two; // Retorna a soma dos dois números
 }
 
-console.log(somarDoisNumeros(56, 95)) // resultado = 151
+// Chama a função com dois números e imprime o resultado no console
+console.log(somarDoisNumeros(56, 95)); // Resultado = 151
 
-
-// Uma variável pode receber uma função
+// Variável que armazena uma função anônima que multiplica um número por 2
 let varFuncao = function (x) {
-    return x*2
+    return x * 2;
 }
 
-console.log(varFuncao(22))
+// Chama a função armazenada na variável e imprime o resultado no console
+console.log(varFuncao(22));
 
+// Função para calcular o fatorial de um número
 function fatorialSimples(parametro) {
-    let fatorial = 1
+    let fatorial = 1;
     
+    // Loop para calcular o fatorial
     for (let i = 1; i <= parametro; i++) {
-        fatorial *= i
+        fatorial *= i;
     }
-    return fatorial
+    return fatorial; // Retorna o fatorial calculado
 }
 
-console.log(fatorialSimples(5))
+// Chama a função para calcular o fatorial de 5 e imprime o resultado no console
+console.log(fatorialSimples(5));
 
-
-// Utilizando recursividade de funções para calcular o fatorial 
+// Função recursiva para calcular o fatorial de um número
 function fatorialRecursiva(parametro) {
-    let fatorial = 1
+    let fatorial = 1;
     
     if (parametro == 1) {
-        return 1
+        return 1; // Caso base: fatorial de 1 é 1
     } else {
-        return parametro * fatorialRecursiva(parametro-1)
+        // Chama recursivamente a função para calcular o fatorial
+        return parametro * fatorialRecursiva(parametro - 1);
     }
 }
 
-console.log(fatorialRecursiva(5))
+// Chama a função recursiva para calcular o fatorial de 5 e imprime o resultado no console
+console.log(fatorialRecursiva(5));
